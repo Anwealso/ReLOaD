19469189c55fb27d268ad1ccbb72d635d0ed84b8
Anwealso <alnicholo@gmail.com>
2023-10-06 11:59:44 +1000

    Added pytorch yolo inference script

---
+++ reload/objectdetection/yolo.py
@@ -0,0 +1,18 @@
+import torch
+import time
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+imgs = ['/home/alex/Pictures/brisbane1.jpg',
+        '/home/alex/Pictures/brisbane2.jpg',
+        '/home/alex/Pictures/brisbane3.jpg',
+        '/home/alex/Pictures/brisbane4.jpg']  # batch of images
+
+# Inference
+results = model(imgs)
+
+# Results
+results.print()
+results.save()  # or .show()
