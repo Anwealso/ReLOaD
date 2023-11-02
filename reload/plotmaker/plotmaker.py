0f80730620e1080d5686d1ff54ae2d0dc564d201
Anwealso <48880195+Anwealso@users.noreply.github.com>
2023-11-02 20:53:25 +1000

    Merged in improved plotmaker changes from results/final-design

--- reload/plotmaker/plotmaker.py
+++ reload/plotmaker/plotmaker.py
@@ -1,74 +1,84 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-array = np.loadtxt('sac_training_reward3.csv', delimiter=',', skiprows=1)
-
-reward = array[:, 2]
-step = array[:, 1]
-max_reward = np.full_like(array[:, 1], 2000)
-
-# # print(array)
-# print()
-# print(reward[0])
-# print(step[0])
-# print(max_reward[0])
-# quit()
-
-font_family = "Helvetica"
-
-
-x = step
-# y = reward
-
 # Create figure and customise formatting
+font_family = "Helvetica"
 plt.ion()
 plt.rc("font", family=font_family)
-
 fig = plt.figure()
-# fig.suptitle(
-#     f"Target Confidence Distributions", color="#333333"
-# )
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
 
+
+# --------------------------------- LOAD DATA -------------------------------- #
+# Series 1
+sac_results = np.loadtxt('run-SAC_0-tag-rollout_ep_rew_mean.csv', delimiter=',', skiprows=1)
+reward_sac = sac_results[:, 2]
+step_sac = sac_results[:, 1]
+print(np.max(reward_sac))
+
+# Series 2
+ppo_results = np.loadtxt('run-PPO_0-tag-rollout_ep_rew_mean.csv', delimiter=',', skiprows=1)
+reward_ppo = ppo_results[:, 2]
+step_ppo = ppo_results[:, 1]
+print(np.max(reward_ppo))
+
+# Series 3
+dqn_results = np.loadtxt('run-DQN_0-tag-rollout_ep_rew_mean.csv', delimiter=',', skiprows=1)
+reward_dqn = dqn_results[:, 2]
+step_dqn = dqn_results[:, 1]
+print(np.max(reward_dqn))
+
+# Series 4
+max_reward = np.full_like(step_sac, 2000)
+
+
+# --------------------------------- PLOT DATA -------------------------------- #
 # Make plots
 series1 = ax.plot(
-    step,
-    reward
+    step_sac,
+    reward_sac
 )
 series2 = ax.plot(
-    step,
+    step_ppo,
+    reward_ppo
+)
+series3 = ax.plot(
+    step_dqn,
+    reward_dqn
+)
+series4 = ax.plot(
+    step_sac,
     max_reward
 )
 
-plt.legend(['SAC Training Reward', 'Max. Available Reward'], loc ="lower right") 
-# plt.legend(['SAC Training Reward', 'Max. Available Reward']) 
-
-# ax.set_xticklabels(x, rotation=90, ha="right")
-
-# Axis formatting.
-ax.set_ylim(0, 2100)
-ax.spines["top"].set_visible(False)
-ax.spines["right"].set_visible(False)
-# ax.spines["left"].set_visible(False)
-# ax.spines["bottom"].set_color("#DDDDDD")
-ax.tick_params(bottom=False, left=False)
-# ax.set_axisbelow(True)
-ax.yaxis.grid(True, color="#EEEEEE")
-ax.xaxis.grid(False)
+plt.legend(['SAC', 'PPO', 'DQN', 'Avg. Max. Available'], loc ="lower right") 
 
 # Add labels and a title.
 ax.set_xlabel(
     "Training Iterations (Millions)", color="#333333"
 )
 ax.set_ylabel(
-    "Average Reward", color="#333333"
+    "Episode Average Reward", color="#333333"
 )
 ax.set_title(
-    f"Episode Average Reward over Training", color="#333333"
+    f"Model Average Reward over Training", color="#333333"
 )
 
+
+# -------------------------------- FORMATTING -------------------------------- #
+# Axis formatting.
+ax.set_ylim(0, 2100)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.spines["bottom"].set_color("#DDDDDD")
+ax.tick_params(bottom=False, left=False)
+# ax.set_axisbelow(True)
+ax.yaxis.grid(True, color="#EEEEEE")
+ax.xaxis.grid(False)
 
 # fig.tight_layout()
 
-plt.savefig("training_reward.png")
+
+# ----------------------------------- SAVE ----------------------------------- #
+plt.savefig("plot.png")
